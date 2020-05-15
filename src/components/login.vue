f18de547a90af1f7850f36e0940f32cf5d630118
xushuaishuai <1244688089@qq.com>
2020-05-15 11:14:47 +0800

    1

--- src/components/login.vue
+++ src/components/login.vue
@@ -10,7 +10,7 @@
             style="width:36px;height:24px;margin-bottom:-3px;"
           />
           &nbsp;
-          <span>高校档案系统1</span>
+          <span>高校档案系统</span>
         </div>
       </el-header>
       <el-main :style="backgroundImage" id="vue-login">
@@ -20,30 +20,32 @@
             <el-col :xs="2" :sm="2" :md="2" :lg="2" :xl="2">&nbsp;</el-col>
             <el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6" class="login-div" id="login-div">
               <div class="layui-form">
-                <ul class="login-type-tab">
-                  <li class="active" style="color: #0BA1DC;font-family:Adobe 黑体;font-size:24px">用户登录</li>
-                </ul>
-                <div class="normal-login-form">
+                <div class="login-container">
                   <el-form
                     :model="ruleForm"
-                    :rules="rules" 
+                    :rules="rules"
                     ref="ruleForm"
-                    label-width="100px"
-                    class="demo-ruleForm"
+                    autocomplete="on" label-position="left"
+                    class="login-form"
                   >
-                    <el-form-item label="活动名称" prop="name">
-                      <el-input v-model="ruleForm.name"></el-input>
+                    <div class="title-container">
+                      <h3 class="title" style="color: #0BA1DC;font-family:Adobe 黑体;font-size:24px">用户登录</h3>
+                    </div>
+
+                    <el-form-item prop="username">
+                      <span class="svg-container"><i class="el-icon-user"></i></span>
+                      <el-input v-model="ruleForm.username" placeholder="用户名"></el-input>
                     </el-form-item>
-                    <el-form-item label="密码" prop="pass">
-                      <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+                    <el-form-item prop="password">
+                       <span class="svg-container"><i class="el-icon-user"></i></span>
+                      <el-input
+                        type="password"
+                        v-model="ruleForm.password"
+                        autocomplete="off"
+                        placeholder="密码"
+                      ></el-input>
                     </el-form-item>
-                    <el-form-item label="密码" prop="pass">
-                      <el-input type="password" v-model="ruleForm.pass1" autocomplete="off"></el-input>
-                    </el-form-item>
-                    <el-form-item>
-                      <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
-                      <el-button @click="resetForm('ruleForm')">重置</el-button>
-                    </el-form-item>
+                    <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click="submitForm('ruleForm')">立即登录</el-button>
                   </el-form>
                 </div>
               </div>
@@ -80,25 +82,22 @@ export default {
         pass: ""
       },
       rules: {
-        name: [
-          { required: true, message: "请输入活动名称", trigger: "blur" },
-          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 1, max: 50, message: "长度在 1 到 50 个字符", trigger: "blur" }
         ],
-        pass: [
+        password: [
           { required: true, message: "请输入密码", trigger: "blur" },
-          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+          { min: 1, max: 50, message: "长度在 1 到 50 个字符", trigger: "blur" }
         ]
       }
     };
   },
   methods: {
-    login: function() {
-      this.$router.push("index");
-    },
     submitForm(formName) {
       this.$refs[formName].validate(valid => {
         if (valid) {
-          alert("submit!");
+          this.$router.push("index");
         } else {
           console.log("error submit!!");
           return false;
@@ -112,9 +111,17 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped >
+
 .grid-content {
   border-radius: 4px;
   min-height: 36px;
 }
+.el-form-item {
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(0, 0, 0, 0.04);
+    border-radius: 5px;
+    color: #454545;
+  }
 </style>
+
